99da2065782fb0173500f58f902b79ab497f2369
Prem-Ray <premanshuray981@gmail.com>
2025-03-26 22:21:16 +0530

    first commit

---
+++ Array/7_Stock_Buy_and_Sell.cpp
@@ -0,0 +1,25 @@
+#include<bits\stdc++.h>
+using namespace std ;
+
+int main(){
+
+    vector<int>prices = {100, 180, 260, 310, 40, 535, 695} ;
+
+    int profit = 0 ;
+    int n = prices.size() ;
+        
+    for(int i=1 ; i<n ; i++){
+        if(prices[i] > prices[i-1]){
+            int newProfit = prices[i]-prices[i-1] ;
+            profit += newProfit ;
+        }
+    }
+
+    cout<<profit<<endl ;
+
+
+
+
+    return 0 ;
+
+}
